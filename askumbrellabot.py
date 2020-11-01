65985530ec4afe07cddcc1f3fb1a7b83405aa03f
Valentim Uliana <valentim.muniz@hotmail.com>
2020-11-01 16:24:35 -0300

    Update askumbrellabot.py

--- askumbrellabot.py
+++ askumbrellabot.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019  Alexandre Argeris
+# Copyright (C) 2020  Alexandre Argeris and Valentim Muniz
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
